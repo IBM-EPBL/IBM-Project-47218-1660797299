a1374e74d5a17ab646810225d5af88938cb892c6
A-JERINA <88337056+A-JERINA@users.noreply.github.com>
2022-11-09 19:31:33 +0530

    Update crop_model.py

--- Project Development Phase/Delivery of Sprint-2/Model Building/CNN MODEL/crop_model.py
+++ Project Development Phase/Delivery of Sprint-2/Model Building/CNN MODEL/crop_model.py
@@ -6,10 +6,12 @@ from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn import model_selection
+
 crop = pd.read_csv('Data/crop_recommendation.csv')
 X = crop.iloc[:,:-1].values
 Y = crop.iloc[:,-1].values
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.15)
+
 models = []
 models.append(('SVC', SVC(gamma ='auto', probability = True)))
 models.append(('svm1', SVC(probability=True, kernel='poly', degree=1)))
@@ -24,13 +26,17 @@ models.append(('knn3', KNeighborsClassifier(n_neighbors=3)))
 models.append(('knn5', KNeighborsClassifier(n_neighbors=5)))
 models.append(('knn7', KNeighborsClassifier(n_neighbors=7)))
 models.append(('knn9', KNeighborsClassifier(n_neighbors=9)))
+
 vot_soft = VotingClassifier(estimators=models, voting='soft')
 vot_soft.fit(X_train, y_train)
 y_pred = vot_soft.predict(X_test)
+
 scores = model_selection.cross_val_score(vot_soft, X_test, y_test,cv=5,scoring='accuracy')
 print("Accuracy: ",scores.mean())
+
 score = accuracy_score(y_test, y_pred)
 print("Voting Score % d" % score)
+
 import pickle
 pkl_filename = 'Crop_Recommendation.pkl'
 Model_pkl = open(pkl_filename, 'wb')
